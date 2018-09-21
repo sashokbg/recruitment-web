4c3c5421afb1f5e3cf37b9beae53605f34e05560
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2018-09-21 10:17:39 +0200

    Added hashmap to book repo

--- src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
+++ src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
@@ -1,8 +1,10 @@
 package fr.d2factory.libraryapp.book;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class BookRepository {
-    private Map<ISBN, Book> availableBooks, borrowedBooks;
+    private Map<ISBN, Book> availableBooks = new HashMap<>();
+    private Map<ISBN, Book> borrowedBooks = new HashMap<>();
 
 }
--- src/test/java/fr/d2factory/libraryapp/library/BorrowBookTest.java
+++ src/test/java/fr/d2factory/libraryapp/library/BorrowBookTest.java
@@ -2,7 +2,7 @@ package fr.d2factory.libraryapp.library;
 
 import org.junit.Test;
 
-public class StudentBorrowBookTest {
+public class BorrowBookTest {
     @Test
     public void student_can_borrow_a_book_if_book_is_present(){
 
