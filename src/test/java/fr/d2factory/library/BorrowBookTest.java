d965a01c858c6026cbf62c294c5ac849d76a46b3
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2018-09-21 09:44:11 +0200

    Added first tests

--- src/test/java/fr/d2factory/library/BorrowBookTest.java
+++ src/test/java/fr/d2factory/library/BorrowBookTest.java
@@ -1,4 +1,20 @@
 package fr.d2factory.library;
 
+import org.junit.Test;
+
 public class BorrowBookTest {
+    @Test
+    public void student_can_borrow_a_book_if_book_is_present(){
+
+    }
+
+    @Test
+    public void when_a_book_is_borrowed_the_borrow_date_is_recorded(){
+
+    }
+
+    @Test
+    public void local_student_can_borrow_a_book_for_a_day(){
+
+    }
 }
--- src/test/java/fr/d2factory/library/ReturnBookTest.java
+++ src/test/java/fr/d2factory/library/ReturnBookTest.java
@@ -2,9 +2,6 @@ package fr.d2factory.library;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-
 public class ReturnBookTest {
     @Test
     public void test(){
